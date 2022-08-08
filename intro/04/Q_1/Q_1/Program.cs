bebb9f4a5e04a8a6c6681dbcf398ba8bc0d0aa09
Rz <ninnk9@gmail.com>
2022-08-08 16:55:14 +0900

    220808_2

---
+++ intro/04/Q_1/Q_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Q_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine(10 + 5);
+            // Console.WriteLine(20 - 10);
+            // Console.WriteLine(4 * 7);
+            // Console.WriteLine(20 / 7);
+            // Console.WriteLine(20 % 7);
+
+            // Console.WriteLine("안녕" + "하세요");
+
+            /*            int number1 = 20;
+                        int number2 = 5;
+                        int number3 = 3;
+                        int number4 = 13;
+                        int number5;
+
+                        number1 = number1 + number2;
+                        number2 = number3 + number4;
+                        number4 = number1 - number4;
+                        number3 = number2 / number4;
+                        number5 = number2 % number4;
+
+                        Console.WriteLine(number1);
+                        Console.WriteLine(number2);
+                        Console.WriteLine(number3);
+                        Console.WriteLine(number4);
+                        Console.WriteLine(number5);*/
+
+            int number1 = 4 - 2;
+            number1 = number1 * 16;
+            number1 = number1 + 2;
+
+            Console.WriteLine("(4 - 2) * 16 + 2");
+            Console.WriteLine(number1);
+        }
+    }
+}
---
+++ intro/04/Q_1/Q_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Q_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int number = 2;
+             number = number * 3;
+             number = number - 4;
+             number = number % 2;
+
+             Console.WriteLine(number);*/
+
+            int coin = 10;
+            int aram = (coin / 2) - 1;
+            coin = coin - aram;
+            Console.WriteLine(aram);
+            int uram = (coin / 2) + 2;
+            coin = coin - uram;
+            Console.WriteLine(uram);
+            Console.WriteLine(coin);
+
+        }
+    }
+}
