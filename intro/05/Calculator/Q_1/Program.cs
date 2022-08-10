e188fc07d6899a9c694225417bff446c715aca39
Rz <ninnk9@gmail.com>
2022-08-10 10:31:19 +0900

    220810

---
+++ intro/05/Calculator/Q_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Q_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("첫 번째 숫자를 입력하세요.");
+            string userInput1 = Console.ReadLine();
+            int number1 = int.Parse(userInput1);
+            Console.WriteLine("두 번째 숫자를 입력하세요.");
+            string userInput2 = Console.ReadLine();
+            int number2 = int.Parse(userInput2);
+            Console.Write(number1);
+            Console.Write(" + ");
+            Console.Write(number2);
+            Console.Write(" = ");
+            Console.WriteLine(number1 + number2);               // 기초 5-1*/
+
+            /*Console.WriteLine("첫 번째 숫자를 입력하세요.");
+            string userInput1 = Console.ReadLine();
+            Console.WriteLine("두 번째 숫자를 입력하세요.");
+            string userInput2 = Console.ReadLine();
+            int number1 = int.Parse(userInput1);
+            int number2 = int.Parse(userInput2);
+
+            Console.WriteLine(number1 - number2);               // 기초 5-2*/
+
+            Console.WriteLine("첫 번째 숫자를 입력하세요.");
+            double number = double.Parse(Console.ReadLine());
+            Console.Write("입력한 숫자는   ");
+            Console.WriteLine(number);                          // 기초 5-3
+
+        }
+    }
+}
