a578f139eeced08818b5954409d448e5042feaff
Rz <ninnk9@gmail.com>
2022-08-25 10:45:07 +0900

    220825_10장문제

--- intro/10/Q_1/Program.cs
+++ intro/10/Q_1/Program.cs
@@ -6,8 +6,50 @@ namespace Q_1
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
+            /*Random random = new Random();
             Console.WriteLine(random.Next(3, 10));
+            Console.WriteLine(random.Next(-2, 4));*/                                                        // 기초 10-1
+
+            /*int lottoLength = 6;
+            int[] lottoNumbers = new int[lottoLength];
+
+            Random random = new Random();
+
+            int lottoNumberIndex = 0;
+            while (lottoNumberIndex < lottoLength)
+            {
+                lottoNumbers[lottoNumberIndex] = random.Next(5, 60);
+
+                bool hasDuplicate = false;
+                for (int i = 0; i < lottoNumberIndex; i++)
+                {
+                    if (lottoNumbers[lottoNumberIndex] == lottoNumbers[i])
+                    {
+                        hasDuplicate = true;
+                        break;
+                    }
+                }
+
+                if (!hasDuplicate)
+                {
+                    lottoNumberIndex++;
+                }
+            }
+
+            Console.Write("로또번호: ");
+            for (int i = 0; i < lottoLength; i++)
+            {
+                Console.Write(lottoNumbers[i]);
+                Console.Write(", ");
+            }
+            Console.WriteLine();*/                                                                         // 기초 10-2
+
+            Random random = new Random();
+
+            int randomNumber = random.Next(8, 50);
+
+            Console.Write("고른숫자: ");
+            Console.WriteLine(randomNumber);                                                                // 기초 10-3
         }
     }
 }
---
+++ intro/10/Q_2/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Q_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("수비수가 1 이상 100이하의 숫자를 무작위로 고릅니다.");
+            Random random = new Random();
+            int Number = random.Next(1, 101);
+            int guesses = 0;
+            Console.WriteLine(Number);
+            while (true)
+            {
+                Console.WriteLine("공격수가 숫자를 입력합니다.");
+                guesses = int.Parse(Console.ReadLine());
+
+                if (guesses > Number)
+                {
+                    Console.WriteLine("더 작은 숫자입니다.");
+                }
+                else if (guesses < Number)
+                {
+                    Console.WriteLine("더 큰 숫자입니다.");
+                }
+                else
+                {
+                    Console.WriteLine("정답입니다.");
+                    break;
+                }
+            }*/                                                                                 // 심화 10-1
+
+            Console.WriteLine("1은 가위, 2는 바위, 3은 보입니다.");
+            Random random = new Random();
+            int RSP = random.Next(1, 4);
+            Console.WriteLine(RSP);
+            int guesses = 0;
+
+            while (true)
+            {
+                Console.WriteLine("1, 2, 3 중에 하나를 고르세요");
+                guesses = int.Parse(Console.ReadLine());
+
+                if (RSP == 1)
+                {
+                    if (guesses == 2)
+                    {
+                        Console.WriteLine("이겼습니다.");
+                        break;
+                    }
+                    else if (guesses == 3)
+                    {
+                        Console.WriteLine("졌습니다.");
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (RSP == 2)
+                {
+                    if (guesses == 1)
+                    {
+                        Console.WriteLine("졌습니다.");
+                        break;
+                    }
+                    else if (guesses == 3)
+                    {
+                        Console.WriteLine("이겼습니다.");
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    if (guesses == 1)
+                    {
+                        Console.WriteLine("이겼습니다.");
+                        break;
+                    }
+                    else if (guesses == 2)
+                    {
+                        Console.WriteLine("졌습니다.");
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
